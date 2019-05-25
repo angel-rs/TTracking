2712619a9691abddfadffd95b8287d4a2216d361
Angel Rodríguez <angelmanuelrs@gmail.com>
2019-05-25 19:02:05 -0400

    [TT-05] - Add Loader component

--- src/screens/Historic/index.js
+++ src/screens/Historic/index.js
@@ -7,6 +7,7 @@ import { SQLite, DangerZone } from 'expo';
 import Layouts from '../../constants/Layouts';
 import Colors from '../../constants/Colors';
 import NavBar from '../../components/NavBar';
+import Loader from '../../components/Loader';
 import { millisecondsToHuman } from '../../utils/Timer';
 
 import styles from './styles';
@@ -43,6 +44,7 @@ class Historic extends Component {
       records: [],
     };
     this.navigation = props.navigation;
+    this.ready = false;
     this.db = null;
   }
 
@@ -88,7 +90,6 @@ class Historic extends Component {
             }
 
             const found = records.findIndex((record) => {
-              console.log(`${record.title} es igual a ${humanTitle}`);
               return record.title === humanTitle;
             });
 
@@ -104,121 +105,132 @@ class Historic extends Component {
             }
           });
 
+          this.ready = true;
           this.setState({ records });
         }
       );
     });
   }
 
-
-  render() {
+  renderContent = () => {
     const { records } = this.state;
 
+    if (!this.ready) {
+      return (
+        <View style={Layouts.center}>
+          <Loader />
+        </View>
+      );
+    }
+
+    if (records.length === 0) {
+      return (
+        <View style={Layouts.center}>
+          <Lottie
+            ref={animation => {
+              this.animation = animation;
+            }}
+            onLayout={() => {
+              this.animation.play();
+            }}
+            style={{ width: 135, height: 135, alignSelf: 'center' }}
+            source={require('../../../assets/animations/empty.json')}
+          />
+
+          <Text style={{ textAlign: 'center', fontSize: 16, color: 'dimgray', marginBottom: 10 }}>
+            Aún no hay nada por aquí...
+          </Text>
+
+          <Button transparent style={{ alignSelf: 'center' }} onPress={() => this.navigation.goBack()}>
+            <Text>
+              Volver
+            </Text>
+          </Button>
+        </View>
+      );
+    }
+
+    return (
+      <SectionList
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item, index, section }) => {
+          const duration = millisecondsToHuman(item.trackedTime, false);
+          const title = `${item.title[0].toUpperCase()}${item.title.substring(1, item.title.length)}`;
+          let color = '#316258';
+
+          if (item.category === 'Trabajo') {
+            color = Colors.categories[0];
+          }
+          if (item.category === 'Estudios') {
+            color = Colors.categories[1];
+          }
+          if (item.category === 'Transporte') {
+            color = Colors.categories[2];
+          }
+          if (item.category === 'Ocio') {
+            color = Colors.categories[3];
+          }
+
+          return (
+            <View
+              key={index}
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                height: 40,
+                paddingVertical: 3,
+                paddingHorizontal: 5,
+                marginHorizontal: 10,
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 5,
+              }}
+            >
+              <View style={{ flex: 0, flexDirection: 'row' }}>
+                <View
+                  style={{
+                    width: 15,
+                    height: 15,
+                    backgroundColor: color,
+                    borderRadius: 7.5,
+                    marginRight: 10,
+                    marginTop: 10,
+                  }}
+                />
+
+                <Text>
+                  {`${title}\n`}
+                  <Text style={{ color: 'gray', fontSize: 14 }}>
+                    { item.category }
+                  </Text>
+                </Text>
+              </View>
+
+              <View style={{ flex: 0 }}>
+                <Text style={{ color: 'gray', fontSize: 14 }}>
+                  { duration }
+                </Text>
+              </View>
+            </View>
+          );
+        }}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 12, marginLeft: 30, marginTop: 5 }}>
+            { title }
+          </Text>
+        )}
+        sections={records}
+      />
+    );
+  }
+
+  render() {
     return (
       <View style={Layouts.container}>
         <NavBar goBack title="Historial" />
 
         <Col style={styles.content}>
-          {
-            records.length === 0
-              ? (
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                  <Lottie
-                    ref={animation => {
-                      this.animation = animation;
-                    }}
-                    onLayout={() => {
-                      this.animation.play();
-                    }}
-                    style={{ width: 135, height: 135, alignSelf: 'center' }}
-                    source={require('../../../assets/animations/empty.json')}
-                  />
-
-                  <Text style={{ textAlign: 'center', fontSize: 16, color: 'dimgray', marginBottom: 10 }}>
-                    Aún no hay nada por aquí...
-                  </Text>
-
-                  <Button transparent style={{ alignSelf: 'center' }} onPress={() => this.navigation.goBack()}>
-                    <Text>
-                      Volver
-                    </Text>
-                  </Button>
-                </View>
-              )
-              : (
-                <SectionList
-                  keyExtractor={(item, index) => item + index}
-                  renderItem={({ item, index, section }) => {
-                    const duration = millisecondsToHuman(item.trackedTime, false);
-                    const title = `${item.title[0].toUpperCase()}${item.title.substring(1, item.title.length)}`;
-                    let color = '#316258';
-
-                    if (item.category === 'Trabajo') {
-                      color = Colors.categories[0];
-                    }
-                    if (item.category === 'Estudios') {
-                      color = Colors.categories[1];
-                    }
-                    if (item.category === 'Transporte') {
-                      color = Colors.categories[2];
-                    }
-                    if (item.category === 'Ocio') {
-                      color = Colors.categories[3];
-                    }
-
-                    return (
-                      <View
-                        key={index}
-                        style={{
-                          flex: 1,
-                          flexDirection: 'row',
-                          height: 40,
-                          paddingVertical: 3,
-                          paddingHorizontal: 5,
-                          marginHorizontal: 10,
-                          alignItems: 'center',
-                          justifyContent: 'space-between',
-                          marginBottom: 5,
-                        }}
-                      >
-                        <View style={{ flex: 0, flexDirection: 'row' }}>
-                          <View
-                            style={{
-                              width: 15,
-                              height: 15,
-                              backgroundColor: color,
-                              borderRadius: 7.5,
-                              marginRight: 10,
-                              marginTop: 10,
-                            }}
-                          />
-
-                          <Text>
-                            {`${title}\n`}
-                            <Text style={{ color: 'gray', fontSize: 14 }}>
-                              { item.category }
-                            </Text>
-                          </Text>
-                        </View>
-
-                        <View style={{ flex: 0 }}>
-                          <Text style={{ color: 'gray', fontSize: 14 }}>
-                            { duration }
-                          </Text>
-                        </View>
-                      </View>
-                    );
-                  }}
-                  renderSectionHeader={({ section: { title } }) => (
-                    <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 12, marginLeft: 30, marginTop: 5 }}>
-                      { title }
-                    </Text>
-                  )}
-                  sections={records}
-                />
-              )
-          }
-
+          { this.renderContent() }
         </Col>
       </View>
     );
